234cbbac8600bf95e07ab154669410bfb4b42da1
Nic Wilson <nicwilson@gmail.com>
2017-07-13 09:54:14 -0500

    Create for_of_loops_octal_binary_values.js

---
+++ for_of_loops_octal_binary_values.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// let categories = ['hardware', 'software', 'vaporware'];
+// for (var item of categories) {
+//   console.log(item); // 'hardware', 'software', 'vaporware'
+// }
+
+// let categories = [,,];
+// for (var item of categories) {
+//   console.log(item); // undefined, undefined
+// }
+
+// let codes = 'ABCDF';
+// let count = 0;
+// for (let code of codes) {
+//   count++;
+// }
+// console.log(count); // 5
+
+// octal values 0o10 = 8
+// binary values 0o10 = 2
